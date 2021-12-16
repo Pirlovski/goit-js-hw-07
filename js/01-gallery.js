d78226b0a850bcca49ecec7a765cb95249491c27
Pirlovski <87051249+Pirlovski@users.noreply.github.com>
2021-12-16 21:12:12 +0300

    1
    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,69 +1,68 @@
-"use strict";
-import images from "./gallery-items.js";
+import { galleryItems } from "./gallery-items.js";
 
-const refs = {
-  gallery: document.querySelector(".js-gallery"),
-  image: document.createElement("img"),
-  lightbox: document.querySelector(".lightbox"),
-  btn: document.querySelector('[data-action="close-lightbox"]'),
-  modal: document.querySelector(".lightbox__content"),
-  lightbox__image: document.querySelector(".lightbox__image"),
+const galleryEL = document.querySelector(".gallery");
+const bodyEl = document.querySelector("body");
+let modalImage;
+
+// 1.Создание и рендер разметки по массиву данных galleryItems
+//   и предоставленному шаблону элемента галереи.
+//   Используй готовый код из первого задания.
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join(" ");
+
+// console.log(galleryItems[0]);
+galleryEL.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// 2 / Реализация делегирования на div.gallery и получение url большого изображения.
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  onOpenModal(e.target.dataset.source);
+}
+galleryEL.addEventListener("click", onImgClick);
+
+//////////////////
+// 1.Определить общего предка группы элементов для отслеживания событий. // gallery //
+//
+// 2 Зарегистрировать на элементе-предке обработчик события которое мы хотим отлавливать от группы элементов.
+//
+// 3 В обработчике использовать event.target для выбора целевого элемента./////////////
+//
+
+// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox
+
+const onConnectionScriptAndStylesModal = (img) =>
+  basicLightbox.create(` <img src = '${img}' width = '2100' alt = '${img}' >`);
+// 4. Открытие модального окна по клику на элементе галереи.
+
+const onOpenModal = (img) => {
+  modalImage = onConnectionScriptAndStylesModal(img);
+  modalImage.show();
+  console.log("Open modal");
+  document.addEventListener("keyup", onKeyPress);
+
 };
 
-const createGalleryItem = ({ preview, original, description }) =>
-  `<li class="gallery__item">
-<a
-  class="gallery__link"
-  href=${original}
->
-  <img
-    class="gallery__image"
-    src=${preview}
-    data-source=${original}
-    alt=${description}
-  />
-</a>
-</li>`;
-const galleryMarkup = images.reduce(
-  (acc, item) => acc + createGalleryItem(item),
-  ""
-);
-refs.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-refs.image.classList.add("gallery__image");
-
-refs.gallery.addEventListener("click", onGalleryClick);
-refs.btn.addEventListener("click", onClickHandlerClose);
-refs.modal.addEventListener("click", closeLightbox);
-
-function onGalleryClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-  if (e.target.nodeName === "IMG") {
-    refs.lightbox.classList.add("is-open");
-    refs.lightbox__image.src = e.target.getAttribute("data-source");
-    refs.lightbox__image.alt = e.target.alt;
-  }
-  window.addEventListener("keyup", clickKey);
-}
-
-function onClickHandlerClose(e) {
-  e.preventDefault(); 
-  refs.lightbox.classList.remove("is-open");
-  refs.lightbox__image.src = '';
-  refs.lightbox__image.alt = '';
-  window.removeEventListener("keyup", clickKey);
-}
-
-function closeLightbox(event) {
-  if (event.target === event.currentTarget) {
-    onClickHandlerClose();
-  }
-}
-
-function clickKey(event) {
-  if (event.code === "Escape") {
-    onClickHandlerClose();
-  }
-}
+const onKeyPress = event => {
+  if (event.code === "Escape") modalImage.close();
+  console.log("Close modal with escape");
+  document.removeEventListener("keyup", onKeyPress);
+  console.log(event);
+};
